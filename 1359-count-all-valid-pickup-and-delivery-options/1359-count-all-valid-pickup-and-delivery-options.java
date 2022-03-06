54e4101a8a8504df81dded4b7b1bf8d4d3f8c31d
Akhil Sharma <54715392+akhil-06@users.noreply.github.com>
2022-03-06 19:49:19 +0530

    Time: 0 ms (100.00%), Space: 38.7 MB (64.02%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -4,15 +4,29 @@
 //SC : O(n)
 class Solution {
     public int countOrders(int n) {
-        long[] dp = new long[501];
-        int mod = (int) Math.pow(10,9) + 7;
-        dp[1]=1L;
-        dp[2]=6L;
-        for (int i=3;i<=n;i++) {
-            int odddNo = 2 * i -1;
-            int permutations = odddNo * (odddNo+1)/2;
-            dp[i] = (dp[i-1]*(permutations))%mod;
+        
+        long ans = 1;
+        int MOD = 1_000_000_007;
+
+        for (int i = 1; i <= n; ++i) {
+            // Ways to arrange all pickups, 1*2*3*4*5*...*n
+            ans = ans * i;
+            // Ways to arrange all deliveries, 1*3*5*...*(2n-1)
+            ans = ans * (2 * i - 1);
+            ans %= MOD;
         }
-        return (int) dp[n];
+        
+        return (int)ans;
+        
+        // long[] dp = new long[501];
+        // int mod = (int) Math.pow(10,9) + 7;
+        // dp[1]=1L;
+        // dp[2]=6L;
+        // for (int i=3;i<=n;i++) {
+        //     int odddNo = 2 * i -1;
+        //     int permutations = odddNo * (odddNo+1)/2;
+        //     dp[i] = (dp[i-1]*(permutations))%mod;
+        // }
+        // return (int) dp[n];
     }
 }
