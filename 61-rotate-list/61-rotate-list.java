7ffc6ddac83a8784f6de0e39e637cc6db62ccafa
Akhil Sharma <54715392+akhil-06@users.noreply.github.com>
2022-03-11 20:52:30 +0530

    Time: 1 ms (75.83%), Space: 43.4 MB (17.95%) - LeetHub

--- 61-rotate-list/61-rotate-list.java
+++ 61-rotate-list/61-rotate-list.java
@@ -15,21 +15,19 @@ class Solution {
 		return head;
 	
 	ListNode copyHead = head;
-	
-	int len = 1;
-	while (copyHead.next != null) {
-		copyHead = copyHead.next;
-		len++;
-	}
-	
-	copyHead.next = head;
-	
-	for (int i = len - k % len; i > 1; i--)
-		head = head.next;
-
-	copyHead = head.next;
-	head.next = null;
-
-	return copyHead;
+        int len = 1;
+         while(copyHead.next != null){
+             copyHead = copyHead.next;
+             len++;
+         }
+        copyHead.next = head;
+        
+        for(int i= len-k %len;i>1;i--)
+            head = head.next;
+        
+        copyHead = head.next;
+        head.next = null;
+        
+        return copyHead;
     }
 }
