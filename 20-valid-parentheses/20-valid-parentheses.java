05db293ce1640fb3193a8b2cb512d36ea9b8e1e4
Akhil Sharma <54715392+akhil-06@users.noreply.github.com>
2022-03-13 19:37:45 +0530

    Time: 3 ms (56.34%), Space: 42.6 MB (9.57%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,24 +1,24 @@
 class Solution {
     public boolean isValid(String s) {
         
-        Stack<Character> st = new Stack<>();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '['){
-                st.push(s.charAt(i));
-            }
-            else if(s.charAt(i) == ')' && !st.isEmpty() && st.peek() == '('){
-                st.pop();
-            }
-            else if(s.charAt(i) == '}' && !st.isEmpty() && st.peek() == '{'){
-                st.pop();
-            }
-            else if(s.charAt(i) == ']' && !st.isEmpty() && st.peek() == '['){
-                st.pop();
-            }else{
+        Stack<Character> stack = new Stack<Character>();
+        // Iterate through string until empty
+        for(int i = 0; i<s.length(); i++) {
+            // Push any open parentheses onto stack
+            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{')
+                stack.push(s.charAt(i));
+            // Check stack for corresponding closing parentheses, false if not valid
+            else if(s.charAt(i) == ')' && !stack.empty() && stack.peek() == '(')
+                stack.pop();
+            else if(s.charAt(i) == ']' && !stack.empty() && stack.peek() == '[')
+                stack.pop();
+            else if(s.charAt(i) == '}' && !stack.empty() && stack.peek() == '{')
+                stack.pop();
+            else
                 return false;
-            }
         }
-        return st.isEmpty();
+        // return true if no open parentheses left in stack
+        return stack.empty();
         
 	// Stack<Character> stack = new Stack<Character>();
 	// for (char c : s.toCharArray()) {
