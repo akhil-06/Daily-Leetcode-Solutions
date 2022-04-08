824a71e38ac60fded0d64f56832e99f5c5ebb41e
Akhil Sharma <54715392+akhil-06@users.noreply.github.com>
2022-04-08 11:30:07 +0530

    Time: 20 ms (56.53%), Space: 52.3 MB (67.99%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
@@ -1,30 +1,51 @@
+// class KthLargest {
+//     private static int k;
+//     private PriorityQueue<Integer> heap;
+    
+//     public KthLargest(int k, int[] nums) {
+//         this.k = k;
+//         heap = new PriorityQueue<>();
+        
+//         for (int num: nums) {
+//             heap.offer(num);
+//         }
+        
+//         while (heap.size() > k) {
+//             heap.poll();
+//         }
+//     }
+    
+//     public int add(int val) {
+//         heap.offer(val);
+//         if (heap.size() > k) {
+//             heap.poll();
+//         }
+
+//         return heap.peek();
+//     }
+// }
+
 class KthLargest {
-    private static int k;
-    private PriorityQueue<Integer> heap;
-    
-    public KthLargest(int k, int[] nums) {
-        this.k = k;
-        heap = new PriorityQueue<>();
-        
-        for (int num: nums) {
-            heap.offer(num);
-        }
-        
-        while (heap.size() > k) {
-            heap.poll();
-        }
-    }
-    
-    public int add(int val) {
-        heap.offer(val);
-        if (heap.size() > k) {
-            heap.poll();
+        final PriorityQueue<Integer> q;
+        final int k;
+
+        public KthLargest(int k, int[] a) {
+            this.k = k;
+            q = new PriorityQueue<>(k);
+            for (int n : a)
+                add(n);
         }
 
-        return heap.peek();
+        public int add(int n) {
+            if (q.size() < k)
+                q.offer(n);
+            else if (q.peek() < n) {
+                q.poll();
+                q.offer(n);
+            }
+            return q.peek();
+        }
     }
-}
-
 /**
  * Your KthLargest object will be instantiated and called as such:
  * KthLargest obj = new KthLargest(k, nums);
