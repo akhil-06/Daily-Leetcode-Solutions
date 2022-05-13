4cc6476f6a91c08567e30ef335ab11af2fda8066
Akhil Sharma <54715392+akhil-06@users.noreply.github.com>
2022-05-13 14:21:19 +0530

    Time: 0 ms (100.00%), Space: 45 MB (40.69%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -33,65 +33,22 @@ class Node {
 //         return root;
    
 //     }
-//     }
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
-
+// }
 class Solution {
     public Node connect(Node root) {
-        if (root == null)
-            return root;
-        
-        Queue<Node> q = new LinkedList();
-        q.offer(root);
-        q.offer(null);
-        //System.out.println(Arrays.toString(q));
-        while(q.size()>1){
-            Node n = q.poll();
-            if(n == null)
-                q.offer(n);
-            else{
-                //System.out.println(n.val+"  "+q.peek()+"    "+n.left.val+"  "+n.right.val);
-                n.next = q.peek();
-                if(n.left != null)
-                    q.offer(n.left);
-                if(n.right != null)
-                    q.offer(n.right); 
-            } 
-            //print(q);
-        }
+        ArrayList<Node> temp=new ArrayList<>();
+        solve(root,temp,0);
         return root;
     }
-    
-    private void print(Queue<Node> q){
-        List<Node> arr = new ArrayList(q);
-        for(Node n: arr){
-            if(n==null){
-                System.out.print("NULL"+"    ");
-                continue;
-            }
-                
-            System.out.print(n.val+"    ");
+    public void solve(Node root,ArrayList<Node> temp,int level){
+        if(root==null)return;
+        if(temp.size()==level)temp.add(root);
+        else{
+            temp.get(level).next=root;
+            temp.set(level,root);
         }
+        solve(root.left,temp,level+1);
+        solve(root.right,temp,level+1);
     }
 }
+
