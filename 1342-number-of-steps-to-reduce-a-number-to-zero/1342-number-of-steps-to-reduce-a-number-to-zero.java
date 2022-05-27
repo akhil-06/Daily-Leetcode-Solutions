6cd7b308ada94e9f20789fb54a7e4ba8944f4f62
Akhil Sharma <54715392+akhil-06@users.noreply.github.com>
2022-05-27 10:36:15 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (95.17%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numberOfSteps (int num) {
+        if(num == 0) {
+            return 0;
+        }
+        else if(num % 2 == 0) {
+            return (numberOfSteps(num/2) + 1);
+        }
+        else if(num % 2 == 1) {
+            return (numberOfSteps(num-1) + 1);
+        }
+        return 0;
+    }
+    
+    // public int numberOfSteps(int num) {
+    //     return num == 0 ? 0 : Integer.bitCount(num) + (31 - Integer.numberOfLeadingZeros(num));
+    // }
+}
