32d1e93b7739277df339e2686bd508ac9360fd2e
Akhil Sharma <54715392+akhil-06@users.noreply.github.com>
2022-06-13 22:58:44 +0530

    Time: 443 ms (6.56%), Space: 50.2 MB (89.17%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  if(x<0) return false;
+    var num = x;
+    var res = 0;
+    while(num>=1){
+        res = (res*10) + (num%10);
+        num = Math.floor(num/10);
+    }
+    return x===res;
+};
