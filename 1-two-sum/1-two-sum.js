73fe3f37c686436ff0ee0fa7b63d7012d41912ff
Akhil Sharma <54715392+akhil-06@users.noreply.github.com>
2022-06-17 12:56:10 +0530

    Time: 267 ms (8.85%), Space: 42.8 MB (38.61%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,13 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let result = [];
-    for(let i=0;i<nums.length;i++){
-        for(let j=i+1;j<nums.length;j++){
-            if(nums[i] + nums[j] === target){
-                result.push(i,j);
-                return result;
-            }
-        }
-    }
+    let ans = [];
+   for(let i=0;i<nums.length-1;i++){
+       for(let j=i+1;j<nums.length;j++){
+           if(nums[i] + nums[j] === target){
+               ans.push(i,j);
+           }
+       }
+   }
+    return ans;
 };
