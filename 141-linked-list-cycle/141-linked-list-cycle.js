1bc9a7d0c6e31f02d5d426dcd9ed67d1dbe4f55a
Akhil Sharma <54715392+akhil-06@users.noreply.github.com>
2022-06-20 13:12:27 +0530

    Time: 115 ms (32.52%), Space: 45.2 MB (33.83%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let fast = head;
+    let slow = head;
+    if(head === null) return false;
+    while(fast && fast.next!=null){
+        slow = slow.next;
+        fast = fast.next.next;
+        
+        if(slow === fast) return true;
+    }
+    return false;
+};
