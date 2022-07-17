ed6af228ff545859bcc6c7147bd99fd226bd26f2
Akhil Sharma <54715392+akhil-06@users.noreply.github.com>
2022-07-17 10:32:35 +0530

    Time: 215 ms (7.23%), Space: 280.8 MB (6.02%) - LeetHub

--- 629-k-inverse-pairs-array/629-k-inverse-pairs-array.java
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.java
@@ -1,20 +1,18 @@
 public class Solution {
+    Integer[][] memo = new Integer[1001][1001];
+    int M = 1000000007;
     public int kInversePairs(int n, int k) {
-        int[][] dp = new int[n + 1][k + 1];
-        int M = 1000000007;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= k && j <= i * (i - 1) / 2; j++) {
-                if (i == 1 && j == 0) {
-                    dp[i][j] = 1;
-                    break;
-                } else if (j == 0)
-                    dp[i][j] = 1;
-                else {
-                    int val = (dp[i - 1][j] + M - ((j - i) >= 0 ? dp[i - 1][j - i] : 0)) % M;
-                    dp[i][j] = (dp[i][j - 1] + val) % M;
-                }
-            }
-        }
-        return dp[n][k];
+        return ((inv(n, k) + M - (k > 0 ? inv(n, k - 1) : 0)) % M);
+    }
+    public int inv(int n, int k) {
+        if (n == 0)
+            return 0;
+        if (k == 0)
+            return 1;
+        if (memo[n][k] != null)
+            return memo[n][k];
+        int val = (inv(n - 1, k) + M - ((k - n) >= 0 ? inv(n - 1, k - n) : 0)) % M;
+        memo[n][k] = (inv(n, k - 1) + val) % M;
+        return memo[n][k];
     }
 }
