043732e134ddf930d9a4e989c356ea89f9a179f7
Akhil Sharma <54715392+akhil-06@users.noreply.github.com>
2022-08-27 11:16:40 +0530

    Time: 17 ms (79.82%), Space: 47.1 MB (82.46%) - LeetHub

---
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String str) {
+        int n = str.length();
+        
+        for (int i = n / 2; i > 0; i--) {
+            if (n % i == 0) {
+                int left = 0;
+                int right = i;
+                
+                while (right < n && str.charAt(left++) == str.charAt(right)) right++;
+                
+                if (right == n) {
+                    return true;
+                }                
+            }
+        }
+        
+        return false;
+    }
+}
