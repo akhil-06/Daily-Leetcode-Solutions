bf006d280b79e87abf3db600dcfc6565d8c03a82
Akhil Sharma <54715392+akhil-06@users.noreply.github.com>
2022-09-12 18:19:58 +0530

    Time: 171 ms (21.81%), Space: 49.4 MB (96.98%) - LeetHub

--- 143-reorder-list/143-reorder-list.js
+++ 143-reorder-list/143-reorder-list.js
@@ -10,21 +10,34 @@
  * @return {void} Do not return anything, modify head in-place instead.
  */
 var reorderList = function(head) {
-    let stack = [], node = head
-  if (!node) return
-  while (node) {
-    stack.push(node)
-    node = node.next
-  }
-
-  let len = stack.length
-  node = head
-  for (let i = 0; i < len; i++) {
-    if (i % 2 === 0)
-      node.next = stack.shift()
-    else
-      node.next = stack.pop()
-    node = node.next
-  }
-  node.next = null
+    
+    //finding the mid part 
+    let slow = head;
+    let fast = head;
+    while(fast.next && fast.next.next){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    
+    //reverse 2nd part 
+    let prev = null;
+    let curr = slow.next;
+    while(curr){
+        let temp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = temp;
+    }
+    slow.next = null;
+    
+    let h1 = head;
+    let h2 = prev;
+    
+    //we are reordering the linkedlist
+    while(h2){
+        let next = h1.next;
+        h1.next = h2;
+        h1 = h2;
+        h2 = next;
+    }
 };
